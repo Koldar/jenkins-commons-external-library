86ab6f560ecade858241893b30824784359166d2
Massimo Bono <massimobono1@gmail.com>
2020-10-13 12:06:32 +0200

    introducing commons

--- vars/gitReattachHead.groovy
+++ vars/gitReattachHead.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 
+def call(final Map data) {
+    return call(data.branchToSet, data.remoteUrl, data.gitExe ?: "git")
+}
+
 def call(String branchToSet, String remoteUrl, String gitExe = "git") {
     echo "Reattach HEAD of git"
     bat(
