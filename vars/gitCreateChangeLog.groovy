0d2213957b1d3581bb8bc993d7549055573527d4
Massimo Bono <massimobono1@gmail.com>
2020-10-13 14:44:21 +0200

    introducing commons

---
+++ vars/gitCreateChangeLog.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+import com.fibonacci.jenkins.commons.StdoutUtils
+
+def call(final Map data) {
+    return call(data.gitExe ?: "git")
+}
+
+def call(String[] entries, String path) {
+    def today = new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC")) 
+
+    def changeLogContent = []
+    changeLogContent.add("CHANGELOG")
+    changeLogContent.add("This file represents the features, bugfix and so on that have been done from tag ${lastTagName} to tag ${tagName}")
+    changeLogContent.add("This file has been generated ${today}")
+    changeLogContent.add("")
+    entries.indexed().collect { index, item -> changeLogContent.add(" - ${index}: ${item}") }
+
+    echo("#lines in changeLogContent = ${changeLogContent.size()}")
+    writeFile(file: path, text: "${changeLogContent.join('\n')}", encoding: "utf8")
+}
