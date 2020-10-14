15cdf927210594004aa1691adec0cb5b9e3777e5
Massimo Bono <massimobono1@gmail.com>
2020-10-14 14:38:34 +0200

    introducing commons

---
+++ vars/executeOnCmd.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+import com.fibonacci.jenkins.commons.StdoutUtils
+import java.lang.Iterable
+import java.util.List
+import java.util.ArrayList
+import java.nio.file.Paths
+
+/**
+ * scan the iterable and filters out duplicat elements
+ *
+ */
+def call(String cmd, Iterable<List<String,String>> environmentVariables, String label) {
+    def actualCmd = ""
+    for (def v: environmentVariables) {
+        actualCmd = actualCmd + "SET ${v[0]}=${v[1]};"
+    }
+    actualCmd = actualCmd + cmd
+    echo "Executing ${actualCmd}"
+    bat(
+        script: actualCmd,
+        label: label
+    )
+    echo "Done Executing ${actualCmd}"
+    
+}
+
+def call(final Map data) {
+    call(data.cmd, data.environmentVariables, data.label)
+}
