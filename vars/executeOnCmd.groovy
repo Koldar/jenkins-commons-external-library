12b600c8efea28bf19efd6b51f7ee3a005ee0b55
Massimo Bono <massimobono1@gmail.com>
2020-10-14 15:32:50 +0200

    introducing commons

--- vars/executeOnCmd.groovy
+++ vars/executeOnCmd.groovy
@@ -10,13 +10,14 @@ import java.nio.file.Paths
  * scan the iterable and filters out duplicat elements
  * ["JAVA_HOME", JAVA_HOME]
  */
-def call(String script, Iterable<List<String>> environmentVariables, String label) {
+def call(String script, Iterable<Iterable<String>> environmentVariables, String label) {
     def actualCmd = ""
     echo "hello!"
     echo "environmentVariables = ${environmentVariables}"
-    for (List<String> v: environmentVariables) {
-        echo "Setting ${v.get(0)} to ${v.get(1)}"
-        actualCmd = actualCmd + "SET ${v.get(0)}=${v.get(1)};"
+    for (Iterable<String> v: environmentVariables) {
+        def l = v.collect()
+        echo "Setting ${v.get(0)} to ${l.get(1)}"
+        actualCmd = actualCmd + "SET ${l.get(0)}=${l.get(1)};"
     }
     actualCmd = actualCmd + script
     echo "Executing ${actualCmd}"
