13d8b4f1836a1b5efbcb6788bb63f27dd912236f
Massimo Bono <massimobono1@gmail.com>
2020-10-24 14:02:36 +0200

    convert into scripted pipeline

--- vars/blueEcho.groovy
+++ vars/blueEcho.groovy
@@ -3,5 +3,7 @@
 import com.fibonacci.jenkins.commons.AnsiColors;
 
 def call(String message) {
-    echo AnsiColors.getColorMessage(message, "blue")
+    ansiColor("vga") {
+        echo AnsiColors.getColorMessage(message, "blue")
+    }
 }
