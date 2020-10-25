26126cc508d52a3a277532efb38c4636f707d974
Massimo Bono <massimobono1@gmail.com>
2020-10-25 16:11:25 +0100

    convert into scripted pipeline

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -4,7 +4,7 @@ def call(final Map data) {
     call(data.remoteUrl, data.branch, data.gitExe, data.removeIfPresent)
 }
 
-def call(String remoteUrl, String branch, bool removeIfPresent = true, String gitExe = "git") {
+def call(String remoteUrl, String branch, boolean removeIfPresent = true, String gitExe = "git") {
     def url = new Url(remoteUrl)
     blueEcho "remove folder ${url}"
     fileOperations {
