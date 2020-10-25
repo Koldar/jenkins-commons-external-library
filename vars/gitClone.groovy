601a0fd891bdd7fe8bc00c3f439037d4d598b7c7
Massimo Bono <massimobono1@gmail.com>
2020-10-25 15:21:04 +0100

    convert into scripted pipeline

---
+++ vars/gitClone.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def call(final Map data) {
+    call(data.remoteurl, data.branch, data.gitExe)
+}
+
+def call(String remoteUrl, String branch, String gitExe = "git") {
+    bat(
+        script: "${gitExe} clone --single-branch --branch ${branch}${remoteUrl}",
+        label: "git clone ${branch} from ${remoteUrl}"
+    )
+}
