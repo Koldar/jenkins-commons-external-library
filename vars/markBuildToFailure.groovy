0bf5ae865d22096ba53d142069c9671e2f9aff02
Massimo Bono <massimobono1@gmail.com>
2020-10-26 10:38:19 +0100

    convert into scripted pipeline

--- vars/markBuildToFailure.groovy
+++ vars/markBuildToFailure.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
 def call() {
-    return call()
-}
-
-def call() {
     currentBuild.result = 'FAILURE'    
 }
--- vars/markBuildToSuccess.groovy
+++ vars/markBuildToSuccess.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
 
-def call() {
-    return call()
-}
 
 def call() {
     currentBuild.result = 'SUCCESS'    
