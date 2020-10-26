0060dbaa6ab8c12d3728176cd4f6d9dac4114288
Massimo Bono <massimobono1@gmail.com>
2020-10-26 11:25:04 +0100

    convert into scripted pipeline

--- src/com/fibonacci/jenkins/commons/version/VisualStudioProjectVersion.groovy
+++ src/com/fibonacci/jenkins/commons/version/VisualStudioProjectVersion.groovy
@@ -38,15 +38,15 @@ class VisualStudioProjectVersion implements IProjectVersion {
             if (propertyGroup.attributes().containsKey("Label") && propertyGroup.attributes().get("Label") == "Versions") {
                 //update version
                 def versionPrefix = propertyGroup.children().find { return it.name().getLocalPart() == "VersionPrefix"}
-                echo("Old VersionPrefix is ${versionPrefix.text()}")
+                //echo("Old VersionPrefix is ${versionPrefix.text()}")
                 versionPrefix.setValue(newVersion)
-                echo("New VersionPrefix is ${versionPrefix.text()}")
+                //echo("New VersionPrefix is ${versionPrefix.text()}")
                 //update version suffix
                 def newSuffix = releaseType.getSuffix()
                 def versionSuffix = propertyGroup.children().find { return it.name().getLocalPart() == "VersionSuffix"}
-                echo("Old VersionSuffix is ${versionSuffix.text()}")
+                //echo("Old VersionSuffix is ${versionSuffix.text()}")
                 versionSuffix.setValue(newSuffix)
-                echo("New VersionSuffix is ${versionSuffix.text()}")
+                //echo("New VersionSuffix is ${versionSuffix.text()}")
                 break
             }
         }
@@ -57,7 +57,7 @@ class VisualStudioProjectVersion implements IProjectVersion {
 
         //ensure the change has been made
         def versionCheck = fetchVersionFromVisualStudio(visualStudioProjectFileXml)
-        echo("When retrying to read the project file, we got a version of ${versionCheck}")
+        //echo("When retrying to read the project file, we got a version of ${versionCheck}")
         if (versionCheck != newVersion) {
             throw new IllegalArgumentException("version ${newVersion} has not been updated!")
         }
